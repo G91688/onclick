d4bd742afd2e1f9a0be3fa551e2de2918de711f9
G91688 <gultacmansimova0493@gmail.com>
2022-10-26 05:32:50 +0200

    onclick

---
+++ js/index.js
@@ -0,0 +1,11 @@
+//alert ("hallo gultac")
+document.write("js dersi <br>")
+document.write("js dersi 2 <br>")
+document.write("js dersi 3 <br>")
+//prompt(" password")
+//confirm("abune olmaq isdeyirsinizmi")
+//console.log (" men burdayam hahaha")
+//document.getElementById("demo2").innerHTML = "esmira"
+function adi_goster(){
+    document.getElementById("demo2").innerHTML = "esmira"    
+}
